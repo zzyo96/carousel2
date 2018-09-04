a1d625166f233dea66e41a1176868e673962113b
zzyo96 <1977795940@qq.com>
2018-09-04 15:28:35 +0800

    无缝轮播

---
+++ main.js
@@ -0,0 +1,40 @@
+let n
+init();
+setInterval(()=>{
+	makeLeave(getImage(n))
+	  .one('transitionend',(e)=>{
+	  	makeEnter($(e.currentTarget))
+	  })
+	 makeCurrent(getImage(n+1))
+	 n+=1
+},3000)
+
+function getImage(n){
+	return $(`.images > img:nth-child(${x(n)})`)
+}
+
+function x(n){
+	if(n>6){
+		n = n%6
+		if(n===0){
+			n=6
+		}
+	}
+	return n
+}
+
+function init(){
+	n = 1
+	$(`.images > img:nth-child(${n})`).addClass('current')
+	  .siblings().addClass('enter')
+}
+
+function makeCurrent($node){
+	return $node.removeClass('enter leave').addClass('current')
+}
+function makeLeave($node){
+	return $node.removeClass('enter current').addClass('leave')
+}
+function makeEnter($node){
+	return $node.removeClass('current leave').addClass('enter')
+}
