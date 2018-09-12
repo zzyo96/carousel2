66118a19d964a1a23f99afa85e9575ddc13a6ae0
zzyo96 <40533543+zzyo96@users.noreply.github.com>
2018-09-12 10:49:25 +0800

    Update main.js
    修改因为切换网页造成的错乱的bug

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let n
 init();
-setInterval(()=>{
+let timer = setInterval(()=>{
 	makeLeave(getImage(n))
 	  .one('transitionend',(e)=>{
 	  	makeEnter($(e.currentTarget))
@@ -8,6 +8,21 @@ setInterval(()=>{
 	 makeCurrent(getImage(n+1))
 	 n+=1
 },2000)
+
+document.addEventListener('visibilitychange',function(e){
+  if(document.hidden){
+    window.clearInterval(timer)
+  }else{
+    timer = setInterval(()=>{
+	makeLeave(getImage(n))
+	  .one('transitionend',(e)=>{
+	  	makeEnter($(e.currentTarget))
+	  })
+	 makeCurrent(getImage(n+1))
+	 n+=1
+},2000)
+  }
+})
 
 function getImage(n){
 	return $(`.images > img:nth-child(${x(n)})`)
